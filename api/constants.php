3b40f7111e8a585f8d0d509204fab33c62208504
Phillip Crumm <pcrumm@p3net.net>
2012-06-20 20:42:52 -0700

    Add HTTP headers to constants

--- api/constants.php
+++ api/constants.php
@@ -13,3 +13,9 @@ define('MODULES_DIR',  API_ROOT . 'modules/');
 // Some constants needed to include phpBB "legally"
 define('PHPBB_ROOT', API_ROOT . '../'); 	// Path to phpBB installation
 define('PHPBB_PHP_EXT', 'php'); 		// PHP extension
+
+// Some HTTP headers
+define('HTTP_VALID', 'HTTP/1.0 200 Request OK');
+define('HTTP_BAD_FORMAT', 'HTTP/1.0 400 Bad Request');
+define('HTTP_UNAUTHORIZED', 'HTTP/1.0 401 Unauthorized');
+define('HTTP_UNIMPLEMENTED', 'HTTP/1.0 501 Not Implemented');
