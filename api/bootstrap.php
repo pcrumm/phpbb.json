f89707541cc886099ccdbefa78d647eca2720989
Phillip Crumm <pcrumm@p3net.net>
2012-06-24 16:37:07 -0700

    Add request and response to our bootstrap, and make all bootstrap includes _once to prevent any issues

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -4,14 +4,21 @@
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 
-include('constants.php');
+include_once('constants.php');
 
 // Setup exceptions
-include(INCLUDES_DIR . 'exceptions/generic.php');
-include(INCLUDES_DIR . 'exceptions/bad_format.php');
-include(INCLUDES_DIR . 'exceptions/internal_error.php');
-include(INCLUDES_DIR . 'exceptions/unauthorized.php');
-include(INCLUDES_DIR . 'exceptions/unimplemented.php');
+include_once(INCLUDES_DIR . 'exceptions/generic.php');
+include_once(INCLUDES_DIR . 'exceptions/bad_format.php');
+include_once(INCLUDES_DIR . 'exceptions/internal_error.php');
+include_once(INCLUDES_DIR . 'exceptions/unauthorized.php');
+include_once(INCLUDES_DIR . 'exceptions/unimplemented.php');
+
+// We need our request and response class, so let's grab them now
+include_once(INCLUDES_DIR . 'request.php');
+include_once(INCLUDES_DIR . 'response.php');
+
+// All of our modules need this, so we'll include it
+include_once(INCLUDES_DIR . 'modules/base.php');
 
 // Setup phpBB
 include('bootstrap_phpbb.php');
