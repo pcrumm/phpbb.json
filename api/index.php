161ee6c03431d86917729a82c74b36b0c4a1fb7d
Phillip Crumm <pcrumm@p3net.net>
2012-06-25 15:55:11 -0700

    The index file that starts the magic

--- api/index.php
+++ api/index.php
@@ -6,3 +6,10 @@
 
 // Set the gears in motion
 include ('bootstrap.php');
+
+// Grab a request object
+$request = new \phpBBJSON\Request($_REQUEST);
+
+// And let the module take care of it
+$module = new \phpBBJSON\Module($request);
+$module->route();
